b6bd7e28e8e01cb4e19b4221c69ffa1aa7bddbc9
Tigran515 <tigikarap@yahoo.com>
2021-12-11 10:56:51 +0400

    💚 ci(classwork) mathematical equation

---
+++ src/com/aca/Main.java
@@ -0,0 +1,54 @@
+package com.aca;
+
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please input your age");
+        int myAge = scanner.nextInt();
+        System.out.println("My age is " +myAge);
+    }
+}
+
+//public class Main {
+//
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("input the first value");
+//        int valuOne = scanner.nextInt();
+//        System.out.println("input the seccond one");
+//        int valuTwo = scanner.nextInt();
+//        int sum = valuOne+valuTwo;
+//
+//        if(sum > 0){
+//            System.out.println("sum < 0 " );
+//        }else{
+//            System.out.println("sum > 0 ");
+//        }
+//    }
+//}
+//
+
+//public class Main {
+//
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("input the first value");
+//        int valuOne = scanner.nextInt();
+//        System.out.println("input the seccond one");
+//        int valuTwo = scanner.nextInt();
+//        int sum = valuOne%valuTwo;
+//        System.out.println("product is" +sum);
+//    }
+//
+//}
+//public class Main {
+//
+//    public static void main(String[] args) {
+//        int intArray
+//    }
+//}
+
