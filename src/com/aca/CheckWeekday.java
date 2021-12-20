d28452c61e60229e7809c2e76f03ec23a6823cca
Tigran515 <tigikarap@yahoo.com>
2021-12-20 22:56:43 +0400

    👷‍♂️ changed(loop) : changed to loop

--- src/com/aca/CheckWeekday.java
+++ src/com/aca/CheckWeekday.java
@@ -3,32 +3,35 @@ package com.aca;
 import java.util.Scanner;
 
 public class CheckWeekday {
-    public static void main(String[] args) {
+    public static void main(String[] args) {    //TASK#7
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a number from 1 to 7");
         int number = scanner.nextInt();               //@TODO: add input-filter, whole number only
 
-        if (number > 0 && number < 8) {                    //TASK#7
-            if (number == 1) {
-                System.out.println("It's Monday");
-            }
-            if (number == 2) {
-                System.out.println("It's Tuesday");
-            }
-            if (number == 3) {
-                System.out.println("It's Wednesday");
-            }
-            if (number == 4) {
-                System.out.println("It's Thursday");
-            }
-            if (number == 5) {
-                System.out.println("It's Friday");
-            }
-            if (number == 6) {
-                System.out.println("It's Saturday");
-            }
-            if (number == 7) {
-                System.out.println("It's Sunday");
+        if (number > 0 && number < 8) {
+
+            switch (number) {
+                case 1:
+                    System.out.println("It's Monday");
+                    break;
+                case 2:
+                    System.out.println("It's Tuesday");
+                    break;
+                case 3:
+                    System.out.println("It's Wednesday");
+                    break;
+                case 4:
+                    System.out.println("It's Thursday");
+                    break;
+                case 5:
+                    System.out.println("It's Friday");
+                    break;
+                case 6:
+                    System.out.println("It's Saturday");
+                    break;
+                case 7:
+                    System.out.println("It's Sunday");
+                    break;
             }
         } else {
             System.out.println("Wrong number. Please,input a number in 1-7 range");
