d56d69cf93cc963e761e98fa799d3549fc228bdd
Tigran515 <tigikarap@yahoo.com>
2021-12-20 22:57:14 +0400

    💚 ci(homework day4) java tasks

---
+++ src/com/aca/RandomNumberGenerator.java
@@ -0,0 +1,30 @@
+package com.aca;
+
+import java.util.Random;
+
+class RandomNumberGenerator {    //TASK#3
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        int rangeStart = 2;
+        int rangeEnd = 7;
+        int mainRange = rangeStart + (rangeEnd - rangeStart + 1);
+        int randomNumber = random.nextInt(mainRange);
+
+        System.out.println("random number is " + randomNumber);
+    }
+
+}
+
+
+
+//class RandomNumberGenerator {
+//
+//    public static void main(String args[]) {
+//        Random rand = new Random();
+//        int range = 10;
+//        int intRandom = rand.nextInt(range);    // bound
+//        System.out.println("Random number is  " + intRandom);
+////
+//    }
+//}
